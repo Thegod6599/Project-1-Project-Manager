9cd5e34812d1cec12b2a166f501de02b672aff14
Thegod6599 <daxp12314@gmail.com>
2026-04-07 15:33:01 +0000

    Bug fix 1

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,17 +1,23 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 function authMiddleware(req, res, next) {
   const token = req.cookies.token;
+
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decoded;
+    req.userId = decoded.userId;
+
     next();
   } catch (err) {
     console.error(err);
     return res.status(401).json({ message: 'Unauthorized' });
   }
 }
-module.exports = (authMiddleware)
+
+module.exports = authMiddleware;
